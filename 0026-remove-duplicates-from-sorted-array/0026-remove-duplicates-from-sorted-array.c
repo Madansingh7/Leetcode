7549b3c3ae8b910fc6cbfe193662d4fc10fd10c2
Madan Singh <madansingh7@yahoo.com>
2025-11-15 18:05:51 +0530

    Time: 0 ms (100%), Space: 12.4 MB (12.65%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.c
@@ -0,0 +1,11 @@
+int removeDuplicates(int* nums, int numsSize) {
+    int n=numsSize, j=0;
+
+    for(int i=1; i<n; i++){
+        if(nums[i]!=nums[j]){
+            j++;
+            nums[j]=nums[i];
+        }
+    }
+    return j+1;
+}
