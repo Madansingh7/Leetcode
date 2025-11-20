1aa798688f2a73de93c21bd3b6d165760d877a93
Madan Singh <madansingh7@yahoo.com>
2025-11-20 17:37:23 +0530

    Time: 0 ms (100%), Space: 9.9 MB (94.35%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.c
@@ -0,0 +1,21 @@
+void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
+    int i=m-1, j=n-1, index=nums1Size-1;
+    while(i>=0&&j>=0){
+        // if nums1 is greater
+        if(nums1[i]>=nums2[j]){
+            nums1[index]=nums1[i];
+            i--;
+            index--;
+        }
+        //if nums2 is greater 
+        else{
+            nums1[index]=nums2[j];
+            index--;
+            j--;
+        }
+    }
+    // if all nums1 elements are higher then nums2 then nums2 remains as it is then just copy it too nums1
+    while(j>=0){
+        nums1[index--]=nums2[j--];
+    }
+}
