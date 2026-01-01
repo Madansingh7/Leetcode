94405fd373a6bbe451b124e2a55f6a86545562d6
Madan Singh <madansingh7@yahoo.com>
2026-01-01 21:29:59 +0530

    Time: 14 ms (12.05%), Space: 8.6 MB (14.01%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+           int count = 0;
+    int odd = 1;
+    while (x >= 0) {
+        x = x - odd;
+        odd = odd + 2;
+        count++;
+    }
+    return count - 1; 
+    }
+};
