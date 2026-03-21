76de9126d4c390609f2d1ac9b08a63a0fe7ad7e8
Madan Singh <madansingh7@yahoo.com>
2026-03-21 16:16:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
     vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        // used max_element stl to find max candies
         auto max = max_element(candies.begin(),candies.end());
+        // it returns iterator 
+
         vector<bool>res;
-        for(int i=0; i<candies.size(); i++){
-            if(candies[i]+ extraCandies >= *max){
-                res.push_back(true);
-            }
-            else{
-                res.push_back(false);
-            }
+
+        // for(int i=0; i<candies.size(); i++){
+        //     if(candies[i]+ extraCandies >= *max){
+        //         res.push_back(true);
+        //     }
+        //     else{
+        //         res.push_back(false);
+        //     }
+        // }
+
+        // this uses for each loop reduces the code
+         for(int c : candies ){
+            res.push_back(c + extraCandies >= *max);
         }
         return res;
     }
