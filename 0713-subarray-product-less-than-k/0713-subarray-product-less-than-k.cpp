9750c46686a0e2599c7723b295ac515803f1cf6c
Madan Singh <madansingh7@yahoo.com>
2026-06-26 21:00:54 +0530

    Time: 0 ms (100%), Space: 65.2 MB (67.58%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        //edge cases
+        if(k==0 || k==1) return 0;
 
-        if(k==0 || k==1)return 0;
+        int left =0, product=1, count=0;
 
-        int left =0;
-        int product =1, count =0;
+        //slinding window logicc
         for(int right=0; right<nums.size(); right++){
-            product *= nums[right];
+            product*=nums[right];
+
+            //if not satisfy condition shrink window else this part not execute and right go on increase
             while(product>=k){
-                product /= nums[left];
+                product/=nums[left];
                 left++;
-            }    
-            count +=right-left+1;
+            }
+            //count every windows no of sub arrays
+            count+=right-left+1;
         }
         return count;
     }
