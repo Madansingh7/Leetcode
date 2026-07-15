841407006c49cf2196a5b67f6b1717f2c99e5363
Madan Singh <madansingh7@yahoo.com>
2026-07-15 11:42:17 +0530

    1423-maximum-points-you-can-obtain-from-cards Medium 0 ms (100%) 46.4 MB (34.17%) Array .cpp 07-15-2026

---
+++ C++/Medium/1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxScore(vector<int>& cardPoints, int k) {
+
+        int sum = 0;
+        int left = k - 1, right = cardPoints.size() - 1;
+        int ans;
+
+        for (int i = 0; i < k; i++) {
+            sum += cardPoints[i];
+        }
+        ans = sum;
+        while (left >= 0) {
+            sum -= cardPoints[left];
+            left--;
+
+            sum += cardPoints[right];
+            right--;
+
+            ans = max(ans, sum);
+        }
+        return ans;
+    }
+};
