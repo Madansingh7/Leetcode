5578026d38d3e14ba1c838a8f78f650a94509dad
Madan Singh <madansingh7@yahoo.com>
2026-07-23 18:19:15 +0530

    0525-contiguous-array Medium 27 ms (39.81%) 88.2 MB (73.13%) Array .cpp 07-23-2026

--- C++/Medium/0525-contiguous-array/0525-contiguous-array.cpp
+++ C++/Medium/0525-contiguous-array/0525-contiguous-array.cpp
@@ -3,25 +3,21 @@ public:
     int findMaxLength(vector<int>& nums) {
 
         unordered_map<int, int> mp;
-        mp[0] =-1;  // Prefix sum 0 before array starts
+        int curSum = 0;
+        mp[0] = -1; // Prefix sum 0 before array starts and index is -1
 
-        int sum =0;
-        int ans =0;
+        int ans=0;
 
-        for (int i =0; i <nums.size(); i++){
-        // Convert 0 -> -1, 1 -> +1
-            if(nums[i] == 0)
-                sum--;
-            else
-                sum++;
+        for (int i = 0; i < nums.size(); i++) {
+            curSum += (nums[i]==1)? 1 : -1;
 
             // If this prefix sum has been seen before
-            if(mp.find(sum) != mp.end()) {
-                ans = max(ans, i - mp[sum]);
+            if (mp.find(curSum) != mp.end()) {
+                ans = max(ans, i - mp[curSum]);
             }
-            // Store only the first occurrence
-            else{
-                mp[sum] = i;
+            // if not store only the first occurrence
+            else {
+                mp[curSum] = i;
             }
         }
         return ans;
