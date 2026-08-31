851e88d6dbfdfb4bded41c6dced1dc2d4ce834a2
Madan Singh <madansingh7@yahoo.com>
2026-08-31 20:36:36 +0530

    0092-reverse-linked-list-ii Medium 0 ms (100%) 43.2 MB (29.2%) Linked List .java 08-31-2026

--- Java/Medium/0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
+++ Java/Medium/0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
@@ -10,36 +10,30 @@
  */
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-
-        if (head == null || left == right) {
+        if(head == null || left==right){
             return head;
         }
 
         ListNode dummy = new ListNode(0);
         dummy.next = head;
 
-        // Reach the node before 'left'
-        ListNode beforeLeft = dummy;
-
-        for (int i = 1; i < left; i++) {
-            beforeLeft = beforeLeft.next;
+        ListNode beforeLeft=dummy;
+        for(int i=1; i<left; i++){
+            beforeLeft=beforeLeft.next;
         }
 
-        // Start reversing
-        ListNode prev = null;
-        ListNode curr = beforeLeft.next;
+        ListNode prev=null;
+        ListNode curr=beforeLeft.next;
 
-        for (int i = 0; i <= right - left; i++) {
+        for(int i=0; i<right-left+1; i++){
             ListNode next = curr.next;
-
-            curr.next = prev;
-            prev = curr;
-            curr = next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
         }
 
-        // Connect the reversed part
         beforeLeft.next.next = curr;
-        beforeLeft.next = prev;
+        beforeLeft.next= prev;
 
         return dummy.next;
     }
