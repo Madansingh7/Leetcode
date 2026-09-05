9775dce30e0a04c26f3b1dad46a14928073a9bb7
Madan Singh <madansingh7@yahoo.com>
2026-09-05 12:35:49 +0530

    0120-triangle Medium 1 ms (99.91%) 45.7 MB (40.34%) Array .java 09-05-2026

--- Java/Medium/0120-triangle/0120-triangle.java
+++ Java/Medium/0120-triangle/0120-triangle.java
@@ -1,30 +1,43 @@
 import java.util.*;
 
 class Solution {
-    public int minimumTotal(List<List<Integer>> triangle) {
-
-        int n = triangle.size();
-
-        int[][] dp = new int[n][n];
-
-        // Base case: copy the last row
-        for(int j = 0; j < n; j++) {
-            dp[n - 1][j] = triangle.get(n - 1).get(j);
+    
+    // Here i is taken as row and j as element in that row 
+    public int minPath(List<List<Integer>> triangle,int i, int j, int[][]dp){
+        //base case
+        if(i == triangle.size()-1){
+            return triangle.get(i).get(j);
         }
 
-        // Move from bottom to top and simultaneously calculating sum 
-        for(int i = n - 2; i >= 0; i--) {
-
-            for(int j = 0; j <= i; j++) {
-
-                int current = triangle.get(i).get(j);
-
-                int left = dp[i + 1][j];
-                int right= dp[i + 1][j + 1];
-
-                dp[i][j] =current + Math.min(left, right);
-            }
+        if(dp[i][j] != Integer.MAX_VALUE){
+            return dp[i][j];
         }
-        return dp[0][0];
+
+        //current value
+        int curr = triangle.get(i).get(j);
+
+        // recursive left
+        int left = minPath(triangle, i+1, j, dp);
+
+        //recursive right
+        int right = minPath(triangle, i+1, j+1, dp);
+
+        dp[i][j] = curr+Math.min(left,right);
+
+        return dp[i][j];
     }
+
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n=triangle.size();
+
+        int [][] dp = new int [n][n];
+
+        // Mark all states as not calculated
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], Integer.MAX_VALUE);
+        }
+
+        return minPath(triangle,0,0, dp);
+    }
+
 }
